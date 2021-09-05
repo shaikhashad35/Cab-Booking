3ff482e18bf8de8b5bf44c9728074575009f1afe
Ashad <shaikhashad35@gmail.com>
2021-09-06 00:41:28 +0530

    First commit

---
+++ src/test/java/com/CabBooking/CabBooking/CabBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CabBooking.CabBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
